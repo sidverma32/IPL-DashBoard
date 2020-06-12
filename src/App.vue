dcf82330d097302ee2783ab713f86976136596b1
sidverma <siddharth.verma@bigbasket.com>
2020-06-12 19:13:06 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <header class="navbar is-dark" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <h1 class="navbar-item has-text-weight-semibold">IPL Dashboard</h1>
+        </div>
+      </div>
+    </header>
+    <router-view class="view"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+  
+</style>
